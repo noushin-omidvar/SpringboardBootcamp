377260dac2710c0d627bde1c73660a459c7e3218
Noushin Omidvar <noush.omidvar@gmail.com>
2022-12-14 10:58:35 -0700

    Update timer.js

--- Exercises/3-3-Timer/timer.js
+++ Exercises/3-3-Timer/timer.js
@@ -15,18 +15,3 @@ function countDown(num){
     }
 
     countDown(4)
-
-    function randomGame(){
-        let i = 0 ;
-        const myInterval = setInterval(function(){
-            let randNum = Math.random();
-            if (randNum > 0.75){
-                clearInterval(myInterval)
-                console.log(i)
-                }
-            else {
-              console.log(randNum);
-            }
-        i++}, 1000)
-    }
-    randomGame();
