5e493292ed3d1c890d4904956f71d41b74ca1e1e
Noushin Omidvar <noush.omidvar@gmail.com>
2023-01-12 13:54:37 -0700

    updates

--- Exercises/10-7-Maps-Sets/Maps-Sets.js
+++ Exercises/10-7-Maps-Sets/Maps-Sets.js
@@ -1,14 +1,13 @@
-const vowelCount = str => {
-    vowels = 'aeiou'
-    onlyVowels = [...str].filter(c=> vowels.includes(c))
-    vowelMap = new Map()
-    for (let c of onlyVowels){
-        if (vowelMap.has(c)){
-            vowelMap.set(c, vowelMap.get(c)+1)  
-        }
-        else{
-            vowelMap.set(c, 1)  
-        }
+const vowelCount = (str) => {
+  vowels = "aeiou";
+  onlyVowels = [...str].filter((c) => vowels.includes(c));
+  vowelMap = new Map();
+  for (let c of onlyVowels) {
+    if (vowelMap.has(c)) {
+      vowelMap.set(c, vowelMap.get(c) + 1);
+    } else {
+      vowelMap.set(c, 1);
     }
-    return vowelMap
-}
+  }
+  return vowelMap;
+};
