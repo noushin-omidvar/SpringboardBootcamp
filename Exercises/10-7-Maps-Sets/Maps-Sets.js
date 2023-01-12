bcd81454f69b4e2fbcdf4a10696f4166375ca3b0
Noushin Omidvar <noush.omidvar@gmail.com>
2023-01-12 13:49:10 -0700

    Solution to 10-7-Sets-Maps Exercise

---
+++ Exercises/10-7-Maps-Sets/Maps-Sets.js
@@ -0,0 +1,14 @@
+const vowelCount = str => {
+    vowels = 'aeiou'
+    onlyVowels = [...str].filter(c=> vowels.includes(c))
+    vowelMap = new Map()
+    for (let c of onlyVowels){
+        if (vowelMap.has(c)){
+            vowelMap.set(c, vowelMap.get(c)+1)  
+        }
+        else{
+            vowelMap.set(c, 1)  
+        }
+    }
+    return vowelMap
+}
