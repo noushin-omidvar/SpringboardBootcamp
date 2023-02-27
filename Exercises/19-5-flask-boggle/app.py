3df37dc981dc8a5021e59ad3ff6363b693c0a980
Noushin Omidvar <noush.omidvar@gmail.com>
2023-02-27 15:11:44 -0800

    Displaying the board and guess form

--- Exercises/19-5-flask-boggle/app.py
+++ Exercises/19-5-flask-boggle/app.py
@@ -1,3 +1,14 @@
+from flask import Flask, request, session, render_template
 from boggle import Boggle
 
+app = Flask(__name__)
+app.secret_key = 'BoGgLe_GaMe'
+
 boggle_game = Boggle()
+
+
+@app.route('/')
+def display_board():
+    board = boggle_game.make_board()
+    session['board'] = board
+    return render_template('board.html')
