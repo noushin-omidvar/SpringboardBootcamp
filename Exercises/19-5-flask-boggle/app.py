36473596e89f6c3a40c51bb0cd230bb6a481470d
Noushin Omidvar <noush.omidvar@gmail.com>
2023-02-28 17:14:36 -0800

    Step 4: Posting a Score

--- Exercises/19-5-flask-boggle/app.py
+++ Exercises/19-5-flask-boggle/app.py
@@ -3,20 +3,39 @@ from boggle import Boggle
 
 app = Flask(__name__, template_folder="templates",
             static_folder='static', static_url_path='/static')
+
+# Set the secret key for the Flask app
 app.secret_key = 'BoGgLe_GaMe'
 
+# Create an instance of the Boggle game
 boggle_game = Boggle()
+
+# Route to display the game board and handle user guesses
 
 
 @app.route('/', methods=['GET', 'POST'])
 def display_board():
 
+    # Handle the submission of a guess
     if request.method == 'POST':
+        # Get the guess from the request data and convert to lowercase
         guess = request.get_json()['guess'].lower()
-        print(guess)
-        print(boggle_game.check_valid_word(board=session['board'], word=guess))
-        return jsonify({'result': boggle_game.check_valid_word(board=session['board'], word=guess)})
+
+        # Check if the guess is a valid word on the board
+        result = boggle_game.check_valid_word(
+            board=session['board'], word=guess)
+
+        # Add to the score if the guess is a valid word on the board
+        if result == 'ok':
+            session['score'] += len(guess)
+
+        # Return the result and current score as a JSON object
+        return jsonify({'result': result, 'score': session['score']})
+
+    # If it's a GET request, initiate the board and score
     board = boggle_game.make_board()
     session['board'] = board
-    print(board)
+    session['score'] = 0
+
+    # Render the game board HTML template
     return render_template('board.html')
--- Exercises/19-5-flask-boggle/static/js/app.js
+++ Exercises/19-5-flask-boggle/static/js/app.js
@@ -1,30 +1,36 @@
-console.log("hello");
+// Initialize a Set to keep track of found words
+let found = new Set();
 
-// res = axios.get("/guess").then(function (response) {
-//   console.log(response);
-// });
+// Listen for a click event on the form submit button
 $("button[type=submit]").on("click", async function (event) {
   event.preventDefault();
-  let formData = {
-    guess: $("#guess").val(),
-  };
-  word = formData["guess"];
+
+  // Get the value of the input field
+  let word = $("#guess").val();
+
+  // Send an AJAX request to the server to check if the word is valid
   await axios
     .post("/", {
-      guess: formData["guess"],
+      guess: word,
     })
     .then(function (response) {
       let result = response.data.result;
-      console.log(result);
-      if (result === "ok") {
-        // handle valid word
+      let score = response.data.score;
+
+      // Check if the word has already been found
+      if (found.has(word)) {
+        $("#result").html(word + " is already found.");
+      } else if (result === "ok") {
+        // Handle the case where the word is valid
+        found.add(word);
         $("#result").html(word + " is a valid word.");
-        $("h4").append(`<li> ${word} </li>`);
+        $("ul").append(`<li> ${word} </li>`);
+        $("#score").text(score + " POINTS");
       } else if (result === "not-on-board") {
-        // handle valid word not on the board
+        // Handle the case where the word is valid but not on the board
         $("#result").html(word + " is not on board.");
       } else {
-        // handle invalid word
+        // Handle the case where the word is not valid
         $("#result").html(word + " is not a valid word.");
       }
     });
