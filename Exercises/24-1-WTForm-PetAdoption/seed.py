7c7454fd5eb026e6841cd0fa933d9b5dfca0eebc
Noushin Omidvar <noush.omidvar@gmail.com>
2023-03-25 00:05:06 -0600

    update seed

--- Exercises/24-1-WTForm-PetAdoption/seed.py
+++ Exercises/24-1-WTForm-PetAdoption/seed.py
@@ -14,12 +14,22 @@ Pet.query.delete()
 lucy = Pet(name="Lucy", species='dog', age=7,
            photo_url="https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/60775111/4/?bust=1678478788&width=720", available=True)
 
+spelt = Pet(name="Spelt", species="cat", age=1,
+            photo_url="https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/60191621/1/?bust=1677529141&width=720", available=True)
+
+
 holmes = Pet(name="Holmes", species="dog", age=5,
              photo_url="https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/60024854/1/?bust=1676989779&width=720")
 
 kittie = Pet(name="Kittie", species="cat", age=2,
-             photo_url="https://unsplash.com/photos/PAcT-amBgr4", available=False)
+             photo_url="https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/60126788/1/?bust=1677354415&width=720", available=False)
+
+cloe = Pet(name="Cloe", species="porcupine", age=1,
+           photo_url="https://animals.sandiegozoo.org/sites/default/files/2016-09/animals_hero_porcupine.jpg", available=False)
+
+sweetie = Pet(name="Sweetie P", species="cat", age=1,
+              photo_url="https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/60982317/5/?bust=1678818983&width=720", available=True)
 
 
-db.session.add_all([lucy, holmes, kittie])
+db.session.add_all([lucy, spelt, holmes, kittie, cloe, sweetie])
 db.session.commit()
