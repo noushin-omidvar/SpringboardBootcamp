ceed1f9955b5a955f64a22572a0d5b238143472c
Noushin Omidvar <noush.omidvar@gmail.com>
2023-03-27 15:31:53 -0600

    Listing, Getting & Creating Cupcakes

--- Exercises/24-3-flask-cupcakes/app.py
+++ Exercises/24-3-flask-cupcakes/app.py
@@ -1,5 +1,5 @@
 """Flask app for Cupcakes"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request
 from models import db, connect_db, Cupcake
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -21,11 +21,49 @@ toolbar = DebugToolbarExtension(app)
 # Database setup
 connect_db(app)
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404_page.html'), 404
+
 # Routes
 
 
 @app.route('/api/cupcakes')
 def get_cupcakes():
+    """Get data about all cupcakes."""
     cupcakes = Cupcake.query.all()
     serialized = [cupcake.serialize() for cupcake in cupcakes]
     return jsonify(cupcakes=serialized)
+
+
+@app.route('/api/cupcakes/<cupcake_id>')
+def get_cupcake(cupcake_id):
+    """Get data about a single cupcake."""
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    serialized = cupcake.serialize()
+    return jsonify(cupcake=serialized)
+
+
+@app.route('/api/cupcakes', methods=['POST'])
+def create_cupcake():
+    """Create a cupcake with flavor, size, rating
+      and image data from the body of the request."""
+
+    flavor = request.json.get('flavor')
+    size = request.json.get('size')
+    rating = request.json.get('rating')
+    image = request.json.get('image')
+
+    print(flavor)
+    new_cupcake = Cupcake(flavor=flavor,
+                          size=size,
+                          rating=rating,
+                          image=image
+                          )
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized = new_cupcake.serialize()
+    return (jsonify(cupcake=serialized), 201)
