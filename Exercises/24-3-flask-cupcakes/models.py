59d69c85b80e440988d3db092645f9cbb9d7aef2
Noushin Omidvar <noush.omidvar@gmail.com>
2023-03-27 13:57:51 -0600

    cupcakes model

--- Exercises/24-3-flask-cupcakes/models.py
+++ Exercises/24-3-flask-cupcakes/models.py
@@ -1,1 +1,34 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+
+class cupcakes(db.Model):
+    """Cupcakes"""
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   auto_increment=True)
+
+    flavor = db.Column(db.String,
+                       nullable=False)
+
+    size = db.Column(db.String,
+                     nullable=False)
+
+    rating = db.Column(db.string,
+                       nullable=False)
+
+    image = db.Column(db.string,
+                      nullable=False,
+                      default="https://tinyurl.com/demo-cupcake")
