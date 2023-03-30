4f6f0711e5658adcb742660b45ddecfe74d56c37
Noushin Omidvar <noush.omidvar@gmail.com>
2023-03-30 15:42:21 -0600

    Added feedbacks form

--- Exercises/24-5-Flask-Feedback/forms.py
+++ Exercises/24-5-Flask-Feedback/forms.py
@@ -18,3 +18,10 @@ class LoginForm(FlaskForm):
 
     username = StringField("Username", validators=[InputRequired()])
     password = PasswordField("Password", validators=[InputRequired()])
+
+
+class FeedbackForm(FlaskForm):
+    """Feedback form"""
+
+    title = StringField("Title", validators=[InputRequired()])
+    content = StringField("Content", validators=[InputRequired()])
